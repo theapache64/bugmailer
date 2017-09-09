e94596109c8265ac6691ac1c0f6f006d3afac81c
theapache64-charlie <theapache64@gmail.com>
2017-09-09 17:00:44 +0530

    Renamed module

--- app/src/main/java/com/theah64/bugmailerexample/MainActivity.java
+++ app/src/main/java/com/theah64/bugmailerexample/MainActivity.java
@@ -14,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Person person = new Person("Anooj", "20");
+
         if (true) {
-            BugMailer.report(new JSONException("Hello"), new Person("Faisal", "20"));
+            BugMailer.report(new JSONException("JSON Failed"), person);
         }
     }
 

7daa111cde20945a6152bf4b362d856ec04bf11f
theapache64-charlie <theapache64@gmail.com>
2018-03-17 09:56:24 +0530

    .backup

--- app/src/main/java/com/theah64/bugmailerexample/App.java
+++ app/src/main/java/com/theah64/bugmailerexample/App.java
@@ -1,6 +1,7 @@
 package com.theah64.bugmailerexample;
 
-import android.app.Application;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.theah64.bugmailer.core.BugMailer;
 import com.theah64.bugmailer.core.BugMailerConfig;
@@ -14,12 +15,12 @@ import org.json.JSONException;
  * Created by theapache64 on 7/9/17.
  */
 
-public class App extends Application {
+public class App extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        MultiDex.install(this);
 
         try {
             BugMailer.init(this,
